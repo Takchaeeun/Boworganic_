dd6bddf68d61298aba0e87a05d4ca016ef8a4e18
minju0726 <minju0726@gmail.com>
2024-03-26 12:45:07 +0900

    모바일메뉴 수정

--- js/from.js
+++ js/from.js
@@ -2,12 +2,26 @@ window.addEventListener("load", function () {
   AOS.init();
   //상단스와이퍼
   var bow = new Swiper(".bow", {
-    spaceBetween:0,
+    // spaceBetween: 30,
     centeredSlides: true,
     loop: true,
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
+    },
+  });
+  // 제품스와이퍼
+  var promotion = new Swiper(".organic", {
+    slidesPerView: 1,
+    spaceBetween: 40,
+    loop: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
   //   top 버튼 스크롤 기능
@@ -35,25 +49,31 @@ window.addEventListener("load", function () {
 
     if (window.scrollY == 0) {
       window.scrollTo({
-        top: 15000,
+        top: 5000,
         behavior: "smooth",
       });
     } else {
       window.scrollTo({
-        top: 15000,
+        top: 5000,
         behavior: "smooth",
       });
     }
   });
 });
-$(document).ready(function () {
-  $(".menu").hide();
-  $(".mb-close").click(() => {
-    $(".menu").fadeOut(500);
-    $(".mb-menu").show();
-  });
-  $(".mb-menu").click(() => {
-    $(".menu").slideToggle(500);
-  });
-});
+$(document).ready(function(){
+  // 화면 크기 변경 시 이벤트를 처리하는 함수
 
+     
+       
+          $(".mb-close").click(() => {
+             
+              $(".mb-menu").show();
+          });
+          $(".mb-menu").click(() => {
+              $(".mbmenu").slideToggle(500);
+          });
+     
+    
+
+
+});
