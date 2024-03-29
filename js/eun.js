68783870a78497a5ae56d0068e9524d2caeb085f
tagchaeeun578 <tagchaeeun578@gmail.com>
2024-03-29 17:07:04 +0900

    모달창에 추가

--- js/eun.js
+++ js/eun.js
@@ -1,27 +1,27 @@
-window.addEventListener("load", function () {
-  const body = document.querySelector("body");
-  const modal = document.querySelector(".modal-wrap");
-  const modalClose = this.document.querySelector(".modal-close");
-  // isOpen 값에 따라 스크롤을 제어하는 함수
-  function controlScroll(isOpen) {
-    if (isOpen) {
-      body.style.overflow = "hidden";
-    } else {
-      body.style.overflow = "auto";
-    }
-  }
+// window.addEventListener("load", function () {
+//   const body = document.querySelector("body");
+//   const modal = document.querySelector(".modal-wrap");
+//   const modalClose = this.document.querySelector(".modal-close");
+//   // isOpen 값에 따라 스크롤을 제어하는 함수
+//   function controlScroll(isOpen) {
+//     if (isOpen) {
+//       body.style.overflow = "hidden";
+//     } else {
+//       body.style.overflow = "auto";
+//     }
+//   }
 
-  // 초기 모달 상태 설정
-  const isOpen = true;
-  controlScroll(isOpen);
+//   // 초기 모달 상태 설정
+//   const isOpen = true;
+//   controlScroll(isOpen);
 
-  modalClose.addEventListener("click", function () {
-    modal.style.display = "none";
+//   modalClose.addEventListener("click", function () {
+//     modal.style.display = "none";
 
-    // 모달이 닫힐 때는 스크롤을 다시 활성화
-    controlScroll(false);
-  });
-});
+//     // 모달이 닫힐 때는 스크롤을 다시 활성화
+//     controlScroll(false);
+//   });
+// });
 
 // 제이쿼리
 $(document).ready(function () {
@@ -65,5 +65,4 @@ $(document).ready(function () {
   });
   // 페이지 로드 시 초기 상품 표시
   $("#load-more").trigger("click");
-  
 });
