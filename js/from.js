8a2f55110bd7dab83aafe79c662c7b3822d566ba
minju0726 <minju0726@gmail.com>
2024-03-29 17:51:06 +0900

    커밋

--- js/from.js
+++ js/from.js
@@ -26,54 +26,22 @@ window.addEventListener("load", function () {
   });
   //   top 버튼 스크롤 기능
   const topBtn = document.getElementById("top-btn");
-  const shopBtn = document.getElementById("shop-btn");
-  shopBtn.addEventListener("click", function (event) {
+  topBtn.addEventListener("click", function (event) {
     event.preventDefault();
     if (window.scrollY == 5000) {
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
-    } else {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
     }
   });
-
-  topBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    console.log(window.scrollY);
-
-    if (window.scrollY == 0) {
-      window.scrollTo({
-        top: 20000,
-        behavior: "smooth",
-      });
-    } else {
-      window.scrollTo({
-        top: 20000,
-        behavior: "smooth",
-      });
-    }
-  });
-});
 $(document).ready(function(){
   // 화면 크기 변경 시 이벤트를 처리하는 함수
-
-     
-       
           $(".mb-close").click(() => {
-             
               $(".mb-menu").show();
           });
           $(".mb-menu").click(() => {
               $(".mbmenu").slideToggle(500);
           });
-     
-    
-
-
+});
 });
